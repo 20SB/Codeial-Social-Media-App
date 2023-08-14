8e01b7c9ceb9d0846b31149f161596556313dac6
20SB <subhabiswal20@gmail.com>
2023-08-14 21:15:07 +0530

    using mongo store to store the session cookie, so that even after server restart it will not ask for sign in again

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 
+const MongoStore = require('connect-mongo')(session);
+
 app.use(express.urlencoded());
 app.use(cookieParser());
 
@@ -31,6 +33,7 @@ app.set('view engine', 'ejs');
 // tell where u have placed views
 app.set('views', './views');
 
+// mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -39,13 +42,21 @@ app.use(session({
     resave: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore({
+        mongooseConnection: db,
+        autoRemove: 'interval', // Use 'interval' instead of 'disabled'
+        autoRemoveInterval: 60 * 24 // This is in minutes (removes expired sessions daily)
+    }, function (err){
+        console.log(err || 'connect-mongodb setup ok');
+    })
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
+
 // use express router for home
 app.use('/', require('./routes'));
 
