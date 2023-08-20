bd7d2bf15566841c7164a0e205a397013ffa54aa
20SB <subhabiswal20@gmail.com>
2023-08-20 13:27:26 +0530

    sorted posts according to recent posts done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,29 +4,31 @@ const User = require("../models/user");
 
 // Controller function for rendering the home page
 module.exports.home = async function (req, res) {
-  try {
-    // Fetch all posts along with their associated user and comments
-    const posts = await Post.find({})
-      .populate("user") // Populate the user field for each post
-      .populate({       // Populate the comments field for each post, and for each comment, populate the user field
-        path: 'comments',
-        populate: {
-          path: 'user'
-        }
-      })
-      .exec(); // Use .exec() to execute the query and return a promise
+    try {
+        // Fetch all posts along with their associated user and comments
+        const posts = await Post.find({})
+            .sort("-createdAt")
+            .populate("user") // Populate the user field for each post
+            .populate({
+                // Populate the comments field for each post, and for each comment, populate the user field
+                path: "comments",
+                populate: {
+                    path: "user",
+                },
+            })
+            .exec(); // Use .exec() to execute the query and return a promise
 
-    // Fetch all users
-    const users = await User.find({}).exec();
+        // Fetch all users
+        const users = await User.find({}).exec();
 
-    // Render the home page and pass the fetched posts and users to the view
-    return res.render("home", {
-      title: "Codeial | Home",
-      posts: posts,
-      all_users: users
-    });
-  } catch (err) {
-    console.log("Error in fetching posts:", err);
-    // Handle the error in an appropriate way, such as rendering an error page
-  }
+        // Render the home page and pass the fetched posts and users to the view
+        return res.render("home", {
+            title: "Codeial | Home",
+            posts: posts,
+            all_users: users,
+        });
+    } catch (err) {
+        console.log("Error in fetching posts:", err);
+        // Handle the error in an appropriate way, such as rendering an error page
+    }
 };
