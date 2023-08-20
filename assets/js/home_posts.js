fe95b0118070c41e4b71f1fd0073a7abfd43021c
20SB <subhabiswal20@gmail.com>
2023-08-20 13:30:13 +0530

    creating a post: receiving data

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,27 +1,67 @@
-{   
+{
     // Function to submit form data for a new post using AJAX.
-    let createPost = function(){
+    let createPost = function () {
         // Select the form element for creating a new post.
-        let newPostForm = $('#new-post-form');
+        let newPostForm = $("#new-post-form");
 
         // Attach a submit event handler to the form.
-        newPostForm.submit(function(e){
+        newPostForm.submit(function (e) {
             e.preventDefault(); // Prevent the default form submission behavior.
             // console.log('clikced')
 
             // Perform an AJAX request to create a new post.
             $.ajax({
-                type: 'post',  // HTTP method for the request.
-                url: '/posts/create',  // URL where the request is sent.
-                data: newPostForm.serialize(),  // Serialize form data for submission.
-                success: function(data){  // Callback on successful response.
-                   console.log(data);  // Log the response data.
-                }, error: function(error){  // Callback on error response.
-                    console.log(error.responseText);  // Log the error response text.
-                }
+                type: "post", // HTTP method for the request.
+                url: "/posts/create", // URL where the request is sent.
+                data: newPostForm.serialize(), // Serialize form data for submission.
+                success: function (data) {
+                    // Callback on successful response.
+                    console.log(data); // Log the response data.
+
+                    let newPost = newPostDom(data.data.post);
+                    $("#posts-list-container>ul").prepend(newPost);
+                },
+                error: function (error) {
+                    // Callback on error response.
+                    console.log(error.responseText); // Log the error response text.
+                },
             });
         });
-    }
+    };
+
+    // method to create a post in DOM
+    let newPostDom = function (post) {
+        return $(`<li class="post mini-container" id="post-${post._id}">
+                    <p>
+                        <a class="delete-post-button" href="/posts/destroy/${post.id}">X</a>
+                    
+                    <!-- Display the post author's name -->
+                    ${post.user.name} <hr>
+                    <!-- Display the post content -->
+                    ${post.content}
+                    </p>
+
+                    <div class="post-comments">
+
+                    <div class="post-comments-list">
+                        <ul id="post-comment-${post._id}">
+                        </ul>
+                    </div>
+
+                    <!-- Display comment form if the user is authenticated -->
+                        <form action="/comments/create" method="post" class="comment-form">
+                        <!-- Input field for entering a new comment -->
+                        <input type="text" name="content" placeholder="Write a comment.." required class="comment-input">
+                        <!-- Hidden field to store the post's ID to which the comment is related -->
+                        <input type="hidden" name="post" value="${post._id}">
+                        <!-- Submit button for adding a new comment -->
+                        <input type="submit" value="Add Comment" class="comment-btn">
+                        </form>
+
+                    
+                    </div>
+                </li>`);
+    };
 
     // Call the createPost function to set up the form submission handling.
     createPost();
