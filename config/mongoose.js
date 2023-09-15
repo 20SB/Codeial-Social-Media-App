61b06ab76dc5c4b50946a6c457c65b790fd96701
20SB <subhabiswal20@gmail.com>
2023-09-15 21:09:25 +0530

    mongoose atlas connected

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,13 @@ const mongoose = require('mongoose');
 const env = require('./environment');
 
 //connect to the database
-mongoose.connect(`mongodb://127.0.0.1/${env.db}`);
+// mongoose.connect(`mongodb://127.0.0.1/${env.db}`);
+const DB = `mongodb+srv://subhabiswal100:QAMqOf8ja2GW9oBL@cluster0.piyhowh.mongodb.net/${env.db}`;
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 //acquire the connection(to check if it's successful)
 const db = mongoose.connection;
